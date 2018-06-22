40b09c4ec98867c929cc4e2d762bd7c073bbe1f5
damianpolak <damianpolak.it@gmail.com>
2018-06-22 15:01:44 +0200

    Update test

--- test.js
+++ test.js
@@ -1,5 +1,5 @@
 import test from 'ava';
-import m from 'superbytes';
+import m from '.';
 
 test('converts bytes to human readable string format', t => {
 	t.is(m(0), '0 B');
@@ -8,10 +8,10 @@ test('converts bytes to human readable string format', t => {
 	t.is(m(10.6), '10.6 B');
 	t.is(m(999), '999 B');
 	t.is(m(1023), '1023 B');
-	t.is(m(1024), '1.00 kB');
-	t.is(m(1025), '1.00 kB');
-	t.is(m(1050), '1.03 kB');
-	t.is(m(345987), '337.88 kB');
+	t.is(m(1024), '1.00 KB');
+	t.is(m(1025), '1.00 KB');
+	t.is(m(1050), '1.03 KB');
+	t.is(m(345987), '337.88 KB');
 	t.is(m(1459872), '1.39 MB');
   t.is(m(20000000), '19.07 MB');
   t.is(m(300000001), '286.10 MB');
