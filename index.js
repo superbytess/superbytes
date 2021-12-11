bde0c5893a2f680556060bd31fc2d51a7a3416f5
Damian Polak <damianpolak.it@gmail.com>
2021-12-11 14:00:51 +0100

    Cosmetic changes

--- index.js
+++ index.js
@@ -16,71 +16,71 @@
 // You can use arg1 and arg2 interchangeably
 
 module.exports = superbytes = (bytes, arg1, arg2) => {
-   'use strict';
+  'use strict';
 
-   const UNITS = ['B', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'];
-   const UNITS2 = ['kB'];
+  const UNITS = ['B', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'];
+  const UNITS2 = ['kB'];
 
-   bytes = Math.abs(bytes);
-   let divider,
-       si,
-       digits = 0;
+  bytes = Math.abs(bytes);
+  let divider,
+    si,
+    digits = 0;
 
-   if((arg1 === undefined) && (arg2 === undefined)) {
-     divider = 1024;
-     si = false;
-     digits = 2;
-   }
-   if(typeof arg1 === 'boolean') {
-     if(arg1) {
-       divider = 1000;
-       si = true;
-     } else {
-       divider = 1024;
-       si = false;
-     }
-     if(typeof arg2 === 'number') {
-       digits = arg2;
-     } else {
-       digits = 2;
-     }
-   } else if(typeof arg1 === 'number') {
-     digits = arg1;
-     if(typeof arg2 === 'boolean') {
-       if(arg2) {
-         divider = 1000;
-         si = true;
-       } else {
-         divider = 1024;
-         si = false
-       }
-     } else {
-       divider = 1024;
-       si = false;
-     }
-   }
+  if (arg1 === undefined && arg2 === undefined) {
+    divider = 1024;
+    si = false;
+    digits = 2;
+  }
+  if (typeof arg1 === 'boolean') {
+    if (arg1) {
+      divider = 1000;
+      si = true;
+    } else {
+      divider = 1024;
+      si = false;
+    }
+    if (typeof arg2 === 'number') {
+      digits = arg2;
+    } else {
+      digits = 2;
+    }
+  } else if (typeof arg1 === 'number') {
+    digits = arg1;
+    if (typeof arg2 === 'boolean') {
+      if (arg2) {
+        divider = 1000;
+        si = true;
+      } else {
+        divider = 1024;
+        si = false;
+      }
+    } else {
+      divider = 1024;
+      si = false;
+    }
+  }
 
-   if(Number.isFinite(bytes)) {
-     if(bytes < divider) {
-       let num = bytes;
-       return `${num} ${UNITS[0]}`;
-     }
+  if (Number.isFinite(bytes)) {
+    if (bytes < divider) {
+      let num = bytes;
+      return `${num} ${UNITS[0]}`;
+    }
 
-     for(let i = 1; i <= 8; i++) {
-       if(bytes >= Math.pow(divider, i) && bytes < Math.pow(divider, i+1)) {
-         let num = (bytes/Math.pow(divider, i)).toFixed(digits);
-         if(si == false) {
+    for (let i = 1; i <= 8; i++) {
+      if (bytes >= Math.pow(divider, i) && bytes < Math.pow(divider, i + 1)) {
+        let num = (bytes / Math.pow(divider, i)).toFixed(digits);
+        if (si == false) {
           return `${num} ${UNITS[i]}`;
-         }
-         console.log(`I: ${i}`);
-         if(si == true && i < 2) {
+        }
+
+        if (si == true && i < 2) {
           return `${num} ${UNITS2[0]}`;
-         }
+        }
 
-         if(si == true && i >= 2) {
+        if (si == true && i >= 2) {
           return `${num} ${UNITS[i]}`;
-         }
-       }
-     }
-   }
- };
+        }
+      }
+    }
+  }
+};
--- test.js
+++ test.js
@@ -1,25 +1,25 @@
 const test = require('ava');
 const superbytes = require('.');
 
-test('converts bytes to human readable string format', t => {
-	t.is(superbytes(0), '0 B');
-	t.is(superbytes(0.6), '0.6 B');
-	t.is(superbytes(10), '10 B');
-	t.is(superbytes(10.6), '10.6 B');
-	t.is(superbytes(999), '999 B');
-	t.is(superbytes(1023), '1023 B');
-	t.is(superbytes(1024), '1.00 KB');
-	t.is(superbytes(1025), '1.00 KB');
-	t.is(superbytes(1050), '1.03 KB');
-	t.is(superbytes(124000, 5), '121.09375 KB');
-	t.is(superbytes(424400, 6), '414.453125 KB');
-	t.is(superbytes(345987), '337.88 KB');
-	t.is(superbytes(1459872), '1.39 MB');
-	t.is(superbytes(5214021, 6), '4.972478 MB')
+test('converts bytes to human readable string format', (t) => {
+  t.is(superbytes(0), '0 B');
+  t.is(superbytes(0.6), '0.6 B');
+  t.is(superbytes(10), '10 B');
+  t.is(superbytes(10.6), '10.6 B');
+  t.is(superbytes(999), '999 B');
+  t.is(superbytes(1023), '1023 B');
+  t.is(superbytes(1024), '1.00 KB');
+  t.is(superbytes(1025), '1.00 KB');
+  t.is(superbytes(1050), '1.03 KB');
+  t.is(superbytes(124000, 5), '121.09375 KB');
+  t.is(superbytes(424400, 6), '414.453125 KB');
+  t.is(superbytes(345987), '337.88 KB');
+  t.is(superbytes(1459872), '1.39 MB');
+  t.is(superbytes(5214021, 6), '4.972478 MB');
   t.is(superbytes(20000000), '19.07 MB');
   t.is(superbytes(300000001), '286.10 MB');
   t.is(superbytes(912394812345), '849.73 GB');
 
-	t.is(superbytes(10000), '9.77 KB');
-	t.is(superbytes(10000, true), '10.00 kB');
+  t.is(superbytes(10000), '9.77 KB');
+  t.is(superbytes(10000, true), '10.00 kB');
 });
